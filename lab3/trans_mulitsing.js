1451800bbb00aaec0b80c5f7e25efd4c1abad75f
hallucination <68617430+llliqwi@users.noreply.github.com>
2023-10-09 11:53:22 +0300

    Update trans_mulitsing.js

--- lab3/trans_mulitsing.js
+++ lab3/trans_mulitsing.js
@@ -5,13 +5,13 @@ const bip32 = require('bip32');
 // Генерация ключей для пользователей 1 и 2
 const user1 =
     bip32.fromBase58(
-        'tprv8ZgxMBicQKsPedjDmSL1f16k11SCrbNgL3Vq7rncdMFbMiu1tvJ7RM5YmCuhMiP8FGHDF5JqJekV8MUqaZQVjnTwu2qD6HkQfrzGKYCWKNy',
+        '',
         TESTNET
     );
 user1.network = TESTNET;
 const user2 =
     bip32.fromBase58(
-        'tprv8ZgxMBicQKsPdBakawX7RQR2T2dNV5cG4BcvZ8ETEdvhgV1j4mjhTsgxoRxHXDea6V4v2JjnTPnMmhCiFH5MWRwJ2yXj9B64y3o1nJB6yyJ',
+        '',
         TESTNET
     );
 user2.network = TESTNET;
