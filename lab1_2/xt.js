0a1b38ab9c839a7e673298673ad362db05699ed3
hallucination <68617430+llliqwi@users.noreply.github.com>
2023-10-09 11:54:49 +0300

    Update xt.js

--- lab1_2/xt.js
+++ lab1_2/xt.js
@@ -4,7 +4,7 @@ const bip32 = require('bip32');
 const TESTNET = bitcoin.networks.testnet;
 
 // Получить WIF из основного расширенного приватного ключа, экспортированного из Copay (отправителя)
-const t = bip32.fromBase58('xprv9s21ZrQH143K3pVh6sUWVMUkgt1zd5LfzVaiFSNA9Nm7a89vuYxMubi6r2k3MLzospkSEyh59JAgfVw6TM4YvjCMNPcuRw2MkmEqsnzTSAz');
+const t = bip32.fromBase58('');
 t.network = TESTNET; // трюк для преобразования ключевой пары mainnet в testnet
 
 // BIP44 (Иерархический определенный кошелек) путь, bitcoin testnet, аккаунт 0, внешний адрес
@@ -19,7 +19,7 @@ const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network:
 var tx = new bitcoin.TransactionBuilder(TESTNET);
 
 //(кто платит)
-var txId = '78f771b024fccc8834263437bf3c5de4979fc84c439191789296bb3ff366e076\n';
+var txId = '\n';
 tx.addInput(txId, 0);
 
 //Определить адрес получателя и сумму (в сатоши)
