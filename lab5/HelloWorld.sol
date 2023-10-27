075dd25ef5180c507129ae741802c6d0c5d105dd
hallucination <68617430+llliqwi@users.noreply.github.com>
2023-10-27 21:01:43 +0300

    Add files via upload

---
+++ lab5/HelloWorld.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public message;
+
+    constructor() {
+        message = "Hello, World!";
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
